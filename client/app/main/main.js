4e2fa113aaaa21aea0e057d0e5a4589e842c269e
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-22 20:47:16 +0530

    client: main: Change controller Name
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -5,6 +5,6 @@ angular.module('thatsMySpotApp')
     $routeProvider
       .when('/', {
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainController'
       });
   });
