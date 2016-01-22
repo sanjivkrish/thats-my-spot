02a1abeee3cfb72438de4c0bf9472affd4d4ab1e
sanjivkrish <sanjiv436.k@gmail.com>
2016-01-22 20:07:21 +0530

    client: app: Add ngAnimate module to the App
    Signed-off-by: sanjivkrish <sanjiv436.k@gmail.com>

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,8 @@ angular.module('thatsMySpotApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ngAnimate'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
